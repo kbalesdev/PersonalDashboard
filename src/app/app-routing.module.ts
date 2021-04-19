4ee5f0b99f823ea455b993de78f6c4235ce3495e
Kyle Bales <kbalesdev@gmail.com>
2021-04-18 21:13:20 -0500

    Added animation when routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { NotesComponent } from './notes/notes.component';
 import { TodosComponent } from './todos/todos.component';
 
 const routes: Routes = [
-  { path: 'bookmarks', component: BookmarksComponent },
-  { path: 'todos', component: TodosComponent },
-  { path: 'notes', component: NotesComponent }
+  { path: 'bookmarks', component: BookmarksComponent, data: { tabIndex: 0 } },
+  { path: 'todos', component: TodosComponent, data: { tabIndex: 1 } },
+  { path: 'notes', component: NotesComponent, data: { tabIndex: 2 } }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('routeAnim', [
+      transition(':increment', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], { optional: true }),
+        group([
+          query(':leave', [
+            animate('300ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(-900px)'
+            }))
+          ], { optional: true }),
+          query(':enter', [
+            style({
+              transform: 'translateX(900px)',
+              opacity: 0
+            }),
+            animate('300ms 120ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], { optional: true })
+        ])   
+      ]),
+
+      transition(':decrement', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], { optional: true }),
+        group([
+          query(':leave', [
+            animate('300ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(900px)'
+            }))
+          ], { optional: true }),
+          query(':enter', [
+            style({
+              transform: 'translateX(-900px)',
+              opacity: 0
+            }),
+            animate('300ms 120ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], { optional: true })
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-personal-dashboard';
+  
+  prepareRoute(outlet: RouterOutlet) {
+    if(outlet.isActivated) {
+      return outlet.activatedRouteData['tabIndex'];
+    }
+  }
 }
